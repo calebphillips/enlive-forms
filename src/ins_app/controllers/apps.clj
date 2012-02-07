21a1c187c807158b857a8c9eba3ec0470a55413f
calebphillips <caleb.phillips@gmail.com>
2012-02-07 16:13:03 -0600

    Add stub handler for posts

--- src/ins_app/controllers/apps.clj
+++ src/ins_app/controllers/apps.clj
@@ -1,5 +1,5 @@
 (ns ins-app.controllers.apps
-  (:use [compojure.core :only [defroutes GET]])
+  (:use [compojure.core :only [defroutes GET POST]])
   (:require [clojure.string :as str]
             [ring.util.response :as ring]
             [ins-app.views.apps :as view]))
@@ -7,5 +7,19 @@
 (defn new-form []
   (view/new-form))
 
+(defn show-success []
+  (view/success))
+
+(defn validate [params]
+  [])
+
+(defn handle-post [params]
+  (let [errors (validate params)]
+    (if (seq errors)
+      (view/new-form-with-errors errors)
+      (ring/redirect "/success"))))
+
 (defroutes routes
-  (GET "/" [] (new-form)))
+  (GET "/" [] (new-form))
+  (GET "/success" [] (show-success))
+  (POST "/" {params :params} (handle-post params)))
--- src/ins_app/views/apps.clj
+++ src/ins_app/views/apps.clj
@@ -1,19 +1,28 @@
 (ns ins-app.views.apps
   (:use [hiccup.core :only [html h]]
-        [hiccup.page-helpers :only [doctype]]
-        [hiccup.form-helpers :only [form-to label text-field submit-button]])
-  (:require [ins-app.views.layout :as layout]))
+        [hiccup.page-helpers :only [doctype]])
+  (:require [ins-app.views.layout :as layout]
+            [hiccup.form-helpers :as fh]))
+
+(defn text-field [id label]
+  (seq [(fh/label id label)
+        (fh/text-field id)]))
 
 (defn app-form []
-  [:form {:method "POST", :action "/" }
-   (label "first-name" "First Name")
-   (text-field "first-name")
-   (label "last-name" "Last Name")
-   (text-field "last-name")
-   [:input {:type "submit" :class "btn-primary" :value "Apply"}]
-   ]
-  )
+  (fh/form-to [:post "/"]
+           (text-field "first-name" "First Name")
+           (text-field "last-name" "Last Name")
+           [:input {:type "submit" :class "btn" :value "Apply"}]))
 
 (defn new-form []
   (layout/common "Insurance App"
                  (app-form)))
+
+(defn new-form-with-errors [errors]
+  (layout/common "Insurance App"
+                 [:h1 "What!"]
+                 (app-form)))
+
+(defn success []
+  (layout/common "Insurance App"
+                 [:h1 "You did it! Stay Strong!"]))
