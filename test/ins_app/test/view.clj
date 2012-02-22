776d2d9f4eaa94d1cf19d7bcbcbd989e5c2a1e91
calebphillips <caleb.phillips@gmail.com>
2012-02-22 15:37:25 -0600

    Removed unused test

--- test/ins_app/test/view.clj
+++ test/ins_app/test/view.clj
@@ -2,8 +2,3 @@
   (:use [ins-app.view])
   (:use [clojure.test]))
 
-(deftest squash-name-into-fields
-  (is (= [{:name :f1 :title "F1" :value :v1 :message :m1}]
-         (squash-name-in {:f1 {:title "F1" :value :v1 :message :m1}}))))
-
-
