e9e9a3e6ffa64a15a0491a3fd1e4726879fb6750
calebphillips <caleb.phillips@gmail.com>
2012-02-22 16:43:38 -0600

    removing persistence code

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
                  [ring/ring-jetty-adapter "0.3.10"]
                  [ring/ring-devel "1.0.2"]
                  [compojure "0.6.4"]
-                 [enlive "1.0.0"]
-                 [korma "0.2.1"]
-                 [postgresql "9.0-801.jdbc4"]
-                 [org.clojure/java.jdbc "0.1.1"]])
+                 [enlive "1.0.0"]])
